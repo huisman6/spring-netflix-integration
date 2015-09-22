d33527e8f6c13a321178de652f1a9b7ae13dcec7
potent6 <potent6@aliyun.com>
2015-09-22 16:11:53 +0800

    url resolver

--- service-cosumer/src/main/java/com/youzhixu/consumer/ConsumerApplication.java
+++ service-cosumer/src/main/java/com/youzhixu/consumer/ConsumerApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lianjia.springremoting.imp.netflix.config.SimpleRPCInvokerConfig;
+import com.lianjia.springremoting.imp.eureka.config.SimpleRPCInvokerConfig;
 import com.lianjia.springremoting.invoker.annotation.Remoting;
 import com.youzhixu.api.service.CityService;
 import com.youzhixu.api.service.UserService;
